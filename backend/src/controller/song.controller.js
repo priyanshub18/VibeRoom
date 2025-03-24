aab0e9d97330ccf24d61609477c70ec735c9932d
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-03-24 13:27:10 +0530

    added-get-trending-song-route

---
+++ backend/src/controller/song.controller.js
@@ -0,0 +1,90 @@
+import { create } from "domain";
+import Song from "../models/song.model.js";
+
+export const getAllSongs = async (req, res, next) => {
+  try {
+    const songs = await Song.find().sort({ createdAt: -1 });
+    res.status(200).json(songs);
+  } catch (error) {
+    console.error("Error on getting all songs: ", error);
+    next(error);
+  }
+};
+
+export const getFeaturedSongs = async (req, res, next) => {
+  try {
+    //find 6 random items using mongoDB aggregate pipelines
+    const songs = await Song.aggregate([
+      {
+        $sample: {
+          size: 6,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          audioUrl: 1,
+          imageUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    console.error("Error on getting featured songs: ", error);
+    next(error);
+  }
+};
+
+export const getMadeForYouSongs = async (req, res, next) => {
+  try {
+    //find 4 random items using mongoDB aggregate pipelines
+    const songs = await Song.aggregate([
+      {
+        $sample: {
+          size: 4,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          audioUrl: 1,
+          imageUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    console.error("Error on getting featured songs: ", error);
+    next(error);
+  }
+};
+
+export const getTrendingSongs = async (req, res, next) => {
+  try {
+    //find 6 random items using mongoDB aggregate pipelines
+    const songs = await Song.aggregate([
+      {
+        $sample: {
+          size: 4,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          audioUrl: 1,
+          imageUrl: 1,
+        },
+      },
+    ]);
+    res.status(200).json(songs);
+  } catch (error) {
+    console.error("Error on getting featured songs: ", error);
+    next(error);
+  }
+};
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middeware.js";
+import { getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
 const router = Router();
 
-router.get("/", getAllSongs);
-router.get("/:id", getSongById);
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/feature", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
